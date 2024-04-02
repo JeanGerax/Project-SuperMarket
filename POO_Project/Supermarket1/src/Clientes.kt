36856e24387a4f50ec927351dd78473f2b389b03
DiogoChipelo05 <diogochipelo05@ua.pt>
2024-04-02 03:16:17 +0100

    Login and Create

--- POO_Project/Supermarket1/src/Clientes.kt
+++ POO_Project/Supermarket1/src/Clientes.kt
@@ -20,9 +20,56 @@ class Clientes(private val clientesFilename: String) {
 //        }
     }
 
+    //Verificar se conta existe
     fun verificarConta(email: String, password: String): Boolean {
         val clienteEncontrado = dados.filterValues { it.containsAll(listOf(email, password)) }
         return clienteEncontrado.isNotEmpty()
     }
-    
+
+    //Criar conta
+    fun criarConta(dados1:List<String> ){
+//        dados += dados1
+        println(dados)
+
+        var comprimento: Int = dados.size
+        if (comprimento > 0) {
+            val ultimaKey = dados.keys.elementAt(comprimento - 1)
+        }
+        comprimento += 1
+        dados += (comprimento.toString() to dados1)
+        println(dados)
+
+        atualizarCSV()
+    }
+
+    //Atualizar no csv com os novos dados
+    private fun atualizarCSV() {
+        val file = File(clientesFilename)
+        if (!file.exists()) {
+            println("Ficheiro CSV não encontrado.")
+            return
+        }
+
+        try {
+            val writer = file.bufferedWriter()
+
+            // Se o arquivo csv estiver vazio é adicionado o cabecalho
+            if (file.length() == 0L) {
+                writer.write("Cod_Cliente, Nome, NIF, Morada, Email, Password")
+                writer.newLine()
+            }
+
+            // Guardar os dados
+            for ((chave, valores) in dados) {
+                val linha = "$chave,${valores.joinToString(",")}"
+                writer.write("$linha\n")
+            }
+
+            writer.close()
+            println("Dados atualizados com sucesso.")
+        } catch (e: Exception) {
+            println("Erro ao atualizar: ${e.message}")
+        }
+    }
+
 }
--- POO_Project/Supermarket1/src/Main.kt
+++ POO_Project/Supermarket1/src/Main.kt
@@ -1,15 +1,16 @@
 fun main() {
     println("Bem Vindo ao SuperMarket!")
 
-    // Caminho para o arquivo CSV de clientes (Por enquanto apenas funciona assim)
+    // Caminho para o ficheiro CSV de clientes (Por enquanto apenas funciona assim)
     val caminho: String = "C:\\Users\\didic\\Documents\\ctesp\\2ºSemestre\\POO_Project\\Supermarket1\\src\\"
 
     var opc: Int
     do {
-        println("-------MENU---------")
-        println("- 1- Clientes      -")
-        println("- 2- Administração -")
-        println("--------------------")
+        println("--------MENU----------")
+        println("- 1- Login Clientes  -")
+        println("- 2- Criar Conta     -")
+        println("- 3- Administração   -")
+        println("----------------------")
         println("")
         println("Digite um número diferente de zero (ou 0 para sair): ")
 
@@ -17,7 +18,8 @@ fun main() {
 
         when (opc) {
             1 -> loginCliente(caminho)
-            2 -> adm()
+            2 -> criarCliente(caminho)
+            3 -> adm()
         }
     } while (opc != 0)
     println("Adeus! Obrigado por visitar o nosso Supermarket!")
@@ -40,6 +42,42 @@ fun loginCliente(caminho: String) {
     } else {
         println("Email ou senha incorretos.")
     }
+}
+
+fun criarCliente(caminho: String) {
+    val clientesBD = Clientes(clientesFilename = caminho + "Clientes.csv")
+    clientesBD.load()
+
+    var nome: String
+    var nif: String
+    var morada: String
+    var email: String
+    var password: String
+    var dados1 = mutableListOf<String>()
+
+    println("---- Criar Conta ----")
+    println("nome?")
+    nome = readLine() ?: ""
+    dados1.add(nome)
+
+    println("nif?")
+    nif = readLine() ?: ""
+    dados1.add(nif)
+
+    println("morada?")
+    morada = readLine() ?: ""
+    dados1.add(morada)
+
+    println("Email?")
+    email = readLine() ?: ""
+    dados1.add(email)
+
+    println("Password?")
+    password = readLine() ?: ""
+    dados1.add(password)
+
+    clientesBD.criarConta(dados1)
+
 }
 
 fun adm() {
