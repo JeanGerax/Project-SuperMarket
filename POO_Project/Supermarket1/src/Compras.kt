111be0b51b988b5b2f727b71bdca6a7b6df39084
JeanGerax <160676813+JeanGerax@users.noreply.github.com>
2024-06-04 18:42:34 +0100

    Update Compras.kt

--- POO_Project/Supermarket1/src/Compras.kt
+++ POO_Project/Supermarket1/src/Compras.kt
@@ -1,9 +1,10 @@
 import java.io.File
+import java.io.IOException
 
 class Compras(private var fileCompras: String) {
     private val dadosCompras = mutableMapOf<String, List<String>>()
     private val dadosPorData = mutableMapOf<String, MutableMap<String,List<String>>>()
-    private val dadosPorVezes = mutableMapOf<String, MutableMap<String,List<String>>>()
+    private val dadosPorVezes = mutableMapOf<String, MutableMap<String,Int>>()
 
 
     fun load():MutableMap<String, List<String>> {
@@ -21,39 +22,39 @@ class Compras(private var fileCompras: String) {
     }
 
 
-    fun dividirPorAnoMes():MutableMap<String, MutableMap<String,List<String>>> {
 
+    fun dividirPorAnoMes(): MutableMap<String, MutableMap<String, List<String>>> {
         val lines = load()
         for (line in lines) {
-            var line2 = line.value
-            var data = line2.getOrNull(1)?.substring(0,7)
-            var data2 = data?.substring(0,7)
-            var keyCompra = line.key
+            val line2 = line.value
+            val data = line2.getOrNull(1)?.substring(0, 7) // Asumiendo que la fecha está en la segunda posición y tiene el formato "AAAA-MM"
+            val keyCompra = line.key
 
             val cliente = line2.getOrNull(0).toString()
-            //Para não haver erro de chave unica, é criada uma variavel com o cliente e a compra
-            var IdFinal = cliente + keyCompra
+            val IdFinal = cliente + keyCompra // Para evitar errores de clave única, creamos una variable con el cliente y la compra
 
-            var produtos = line2.drop(n=2)
-            val clienteProduto = mutableMapOf(
-                IdFinal to produtos
-            )
+            val produtos = line2.drop(2) // Saltamos las primeras dos posiciones que contienen el cliente y la fecha
+            val clienteProduto = mapOf(IdFinal to produtos)
 
-            if (dadosPorData.containsKey(data)) {
-                //Se o ano-mes existir adiciona aos dados ja existintes outro cliente e produtos
-                dadosPorData[data!!]?.putAll(clienteProduto)
-
-            } else {
-                //Se o ano-mes nao existir adiciona juntamente com o cliente e produtos
-                dadosPorData[data!!] = clienteProduto
+            if (data != null) {
+                if (dadosPorData.containsKey(data)) {
+                    // Si el año-mes existe, agregamos al mapa existente el cliente y los productos
+                    dadosPorData[data]?.putAll(clienteProduto)
+                } else {
+                    // Si el año-mes no existe, creamos un nuevo mapa para ese año-mes y agregamos el cliente y los productos
+                    dadosPorData[data] = mutableMapOf(IdFinal to produtos)
+                }
             }
         }
         return dadosPorData
     }
 
 
+    // vezesClientesCompraramNumMes
 
- fun mostrarPorIdasAoSupermercadoNoAnoMes_v2(): MutableMap<String, MutableMap<String, Int>> {
+
+    // versao feita por Diogo
+    fun mostrarPorIdasAoSupermercadoNoAnoMes_v2(): MutableMap<String, MutableMap<String, Int>> {
 
         val lines = load()
         for (line in lines) {
@@ -78,3 +79,19 @@ class Compras(private var fileCompras: String) {
         }
         return dadosPorVezes
     }
+
+    fun buscarPorNumeroProductosComprados(): Map<String, Int> {
+        val numProductosComprados = mutableMapOf<String, Int>()
+        dadosCompras.values.forEach { productos ->
+            productos.drop(3).forEach { producto ->
+                numProductosComprados[producto] = numProductosComprados.getOrDefault(producto, 0) + 1
+            }
+        }
+        return numProductosComprados
+    }
+
+    fun pesquisarProdutoNoAnoMes(anoMes: String): List<String>? {
+        val comprasPorData = dadosCompras.filter { it.value[2].startsWith(anoMes) }
+        return comprasPorData.values.flatten().toList()
+    }
+}
