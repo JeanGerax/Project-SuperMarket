15bcd1ffb90b33f93b0136bed8933d52c9434995
JeanGerax <160676813+JeanGerax@users.noreply.github.com>
2024-06-04 18:42:06 +0100

    Update Main.kt

--- POO_Project/Supermarket1/src/Main.kt
+++ POO_Project/Supermarket1/src/Main.kt
@@ -6,7 +6,7 @@ fun main() {
         limparConsole()
         println("Segmentação de Clientes de um Supermercado!\n")
 
-        var file = Compras(fileCompras = "src\\compras.csv")
+        var file = Compras(fileCompras = "C:\\Users\\gerem\\OneDrive\\Escritorio\\U.A\\SuperMarket\\SuperMarket1\\src\\compras.csv")
         var dadosC = file.load()
 
         println("--------------------Menu-------------------")
@@ -29,6 +29,31 @@ fun main() {
                 val listaPorData = file.mostrarPorIdasAoSupermercadoNoAnoMes_v2()
                 mostrarIdasAoSuperAnoMes(listaPorData)
             }
+
+            3 -> {
+                val listaPorData = file.buscarPorNumeroProductosComprados()
+                println("\nNúmero de produtos comprados:")
+                listaPorData.forEach { (producto, cantidad) ->
+                    println("Produto: $producto, Cantidad: $cantidad")
+                }
+            }
+            4 -> {
+                println("Digite o ano e mês (no formato AAAA-MM) para pesquisar produtos:")
+                print("Ano-Mês: ")
+                val anoMes = readLine()?.trim()
+                if (anoMes != null) {
+                    val produtosNoAnoMes = file.pesquisarProdutoNoAnoMes(anoMes)
+                    if (produtosNoAnoMes != null) {
+                        println("\nProdutos comprados no mês $anoMes:")
+                        produtosNoAnoMes.forEach { println(it) }
+                    } else {
+                        println("Não há dados disponíveis para o ano e mês especificados.")
+                    }
+                } else {
+                    println("Formato de data inválido.")
+                }
+            }
+
             0 -> println("Adeus!")
             else -> println("Opção inválida! Tente novamente.")
         }
@@ -61,6 +86,7 @@ fun limparConsole() {
     }
 }
 
+// vezesClientesCompraramNumMes
 fun mostrarIdasAoSuperAnoMes(listaPorData: MutableMap<String, MutableMap<String, Int>>){
 
     for ((data, clientes) in listaPorData) {
@@ -79,4 +105,37 @@ fun mostrarIdasAoSuperAnoMes(listaPorData: MutableMap<String, MutableMap<String,
     }
     println("\nDigite qualquer tecla para voltar...")
     readLine()
+}
+
+
+
+// tens de verificar, porque não procura por formato de data.
+fun mostrarPorNumeroProductosComprados(listaPorData: MutableMap<String, MutableMap<String, List<String>>>) {
+    println("Digite o ano e mês (no formato AAAA-MM) para ver o número de produtos comprados:")
+    print("Ano-Mês: ")
+    val anoMes = readLine()?.trim()
+
+    if (anoMes != null && listaPorData.containsKey(anoMes)) {
+        val comprasPorData = listaPorData[anoMes]
+        val numProductosComprados = mutableMapOf<String, Int>()
+
+        comprasPorData?.forEach { (_, productos) ->
+            val productosSinInfo = productos.drop(3) // Se omite la información de ID, ID_Cliente y Data
+            productosSinInfo.forEach { producto ->
+                numProductosComprados[producto] = numProductosComprados.getOrDefault(producto, 0) + 1
+            }
+        }
+
+        println("\nNúmero de produtos comprados no mês $anoMes:")
+        numProductosComprados.forEach { (producto, cantidad) ->
+            println("Produto: $producto, Cantidad: $cantidad")
+        }
+    } else {
+        println("Não há dados disponíveis para o ano e mês especificados.")
+    }
+
+    println("\nDigite qualquer tecla para voltar...")
+    readLine()
+
+
 }
